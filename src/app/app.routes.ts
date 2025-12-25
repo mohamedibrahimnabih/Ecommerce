c94623b062c07cb9bc994458e74e34491be311ce
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2025-12-25 11:39:14 +0400

    Add New Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { Privacy } from './privacy/privacy';
+import { AboutUs } from './about-us/about-us';
 
 export const routes: Routes = [
-  { path: '/privacy', component: Privacy }
+  { path: 'Privacy', component: Privacy },
+  { path: 'AboutUs', component: AboutUs },
+  {path: '', redirectTo: '/Home', pathMatch: 'full' }
 ];
