7677c9b69114305e46a250ef1be47f4165c9c363
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2025-12-25 11:46:20 +0400

    Not Found Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { Privacy } from './privacy/privacy';
 import { AboutUs } from './about-us/about-us';
+import { Home } from './home/home';
+import { PageNotFound } from './page-not-found/page-not-found';
 
 export const routes: Routes = [
   { path: 'Privacy', component: Privacy },
   { path: 'AboutUs', component: AboutUs },
-  {path: '', redirectTo: '/Home', pathMatch: 'full' }
+  { path: '', component: Home },
+  { path: '**', component: PageNotFound }
 ];
